d3c52dd0f650de668c1749173a0ac05ff3f7dfbf
Andrew Oeuvray <qhx398@hotmail.com>
2018-10-24 23:01:10 +0000

    Core Setup

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-var express = require('express');
-var app = express();
+//DEPENDANCIES
+var express = require('express'),
+    app = express();
 
+
+//SETUP
+app.set('view engine', 'ejs');
+//ALLOW EJS TEMPLATE TO USE PUBLIC FOLDER
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
+//ROUTES
 app.get('/', function(req, res) {
-    res.send('HELLO WORLD THIS IS MY FIRST NODE APP');
+    res.render('index');
 });
 
+//KEEP UNDEFINED ROUTE AT THE BOTTOM TO NOT INTEFERE WITH VALID ROUTES!
+app.get('*', function(req, res) {
+   res.send('NOTHING TO SEE HERE :)'); 
+});
+
+
+
+
+//START SERVER
 if (process.env.PORT == '8080') {
     app.listen(process.env.PORT, console.log('NODEAPP IS LISTENING ON PORT ' + process.env.PORT + '!'));
 } else {
